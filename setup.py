12308ead8328d8f0187f6ef6bb62b545c3072c2c
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-11-17 21:12:31 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pms7003",
+    version="0.0.0",
+    author="Tomasz Lewicki",
+    author_email="t.w.lewicki@gmail.com",
+    description="A python driver for plantower pms7003 pm sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomek-l/pms7003",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'pyserial',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
